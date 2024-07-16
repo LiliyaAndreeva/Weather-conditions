8a4b3207fd6a1b836759773f31ee407ed2f76c7f
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2024-07-16 17:34:24 +0500

    Initial Commit

---
+++ Weather conditions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather conditions
+//
+//  Created by Лилия Андреева on 16.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
